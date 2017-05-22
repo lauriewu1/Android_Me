1bf737d054a9acbb715002a3f0b85eb68abcf2ba
Laurie <lauriewu1@gmail.com>
2017-05-21 22:57:23 -0700

    Create Body Part Fragment

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.android_me.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Laurie on 5/21/2017.
+ */
+
+public class BodyPartFragment extends Fragment {
+}
