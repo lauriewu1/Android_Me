1a52cde9f4d4210fe1f3016f57d3e6f10822652b
Laurie <lauriewu1@gmail.com>
2017-05-26 14:08:36 -0700

    create fragment_master_list layout ro display all images in grid view; create MasterListFragment to display the gridview list; In MasterLIstFragment create a new MasterListAdapter object and set it on a the gridview object; create MainActivity and activity_main layout to display a MasterListFragment; declare the MainActivity in the manifest.

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.android_me.R;
+
+/**
+ * Created by Laurie on 5/26/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -0,0 +1,41 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+/**
+ * Created by Laurie on 5/25/2017.
+ */
+
+public class MasterListFragment extends Fragment{
+
+    //Mandatory empty constructor
+    public MasterListFragment(){
+    }
+
+    //Inflates the GridView of all AndroidMe images
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        final View rootView = inflater.inflate(R.layout.fragment_master_list,container,false);
+
+        //get a reference to the GridView in the fragment_master_list xml layout file
+        GridView gridView = (GridView) rootView.findViewById(R.id.images_grid_view);
+
+        //create the adapter, this adapter takes in the context and an ArrayList of all of the
+        //image resources to display
+        MasterListAdapter mAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
+
+        //set the adapter on the GridView
+        gridView.setAdapter(mAdapter);
+
+        return rootView;
+    }
+}
