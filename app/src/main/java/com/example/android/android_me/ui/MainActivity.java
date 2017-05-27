94e33d2b0ceda74d0a11c908839da31643dce86f
Laurie <lauriewu1@gmail.com>
2017-05-26 19:00:43 -0700

    change fragment_master_list layout to linear layout with the grid view and a next button; add code to do simple math to calculate and store selected list index for head, body, and leg; put the index information into bundle and attach it to an intent that will launch an AndroidMeActivity; get reference to next button and launch the intent when this button is clicked; retrieve bundle list index and use them to display the body part images on AndroidMeActivity.

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -16,8 +16,11 @@
 
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
@@ -26,6 +29,11 @@ import com.example.android.android_me.R;
 // Implement the MasterListFragment callback, OnImageClickListener
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
 
+    //variables to store the values for the list index of the selected images,
+    // the default value will be 0;
+    private int headIndex;
+    private int bodyIndex;
+    private int legIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +47,47 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
         // Create a Toast that displays the position that was clicked
         Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
 
-        // TODO (2) Based on where a user has clicked, store the selected list index for the head, body, and leg BodyPartFragments
+        // Based on where a user has clicked, store the selected list index for the head, body, and leg BodyPartFragments
+        //based on where a user has clicked, store that list index for the head, list, and leg BodyPartFragments
+        //bodyPartNumber will be = 0 for head fragment, 1 for the body, and 2 for the leg because
+        //Dividing by 12 gives us these integer values since each list of images resources has size of 12
+        int bodyPartNumber = position/12; //keep the whole number drop off the decimals
 
-        // TODO (3) Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
+        //Store the correct list index no matter where in the image list has been clicked
+        //this ensures that the index will always be a value between 0-11, so we can match it
+        // to the individual body part lists
+        int listIndex = position - 12*bodyPartNumber;
 
-        // TODO (4) Get a reference to the "Next" button and launch the intent when this button is clicked
+        //set the currently displayed item for the correct body part fragment
+        switch(bodyPartNumber){
+            case 0: headIndex = listIndex;
+                break;
+            case 1: bodyIndex = listIndex;
+                break;
+            case 2: legIndex = listIndex;
+                break;
+            default:break;
 
+        }
+
+        // Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
+        Bundle b = new Bundle();
+        b.putInt("headIndex", headIndex);
+        b.putInt("bodyIndex", bodyIndex);
+        b.putInt("legIndex", legIndex);
+
+        //this launches a new AndroidMeActivity
+        final Intent intent = new Intent(this, AndroidMeActivity.class);
+        intent.putExtras(b);
+
+        // Get a reference to the "Next" button and launch the intent when this button is clicked
+        Button nextButton = (Button) findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(intent);
+            }
+        });
     }
 
 }
